053d99064b5d33b3e734c35682be90be833c12c5
TI Motopema <ti@motopema.com.br>
2026-03-06 14:22:16 -0300

    fix: correct configuration import on the server

--- app/server.py
+++ app/server.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.routers.bitrix import router as bitrix_router
-from core import settings
+from app.core.config import settings
 
 app = FastAPI(
     title=settings.APP_NAME,
